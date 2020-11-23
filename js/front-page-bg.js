545191979cb6cf6fd50e28e9cb552bd9cddb4e63
irina <irina.konovalova455@gmail.com>
2020-11-23 11:37:42 +0200

    front page gallery

---
+++ js/front-page-bg.js
@@ -0,0 +1,98 @@
+const imagesArray = [
+
+  {
+    img: 'images/front-page/placeholder1.jpg',
+  },
+
+  {
+    img: 'images/front-page/placeholder2.jpg',
+  },
+
+  {
+    img: 'images/front-page/placeholder3.jpg',
+  },
+
+  {
+    img: 'images/front-page/placeholder4.jpg',
+  },
+
+  {
+    img: 'images/front-page/placeholder5.jpg',
+  },
+
+  {
+    img: 'images/front-page/placeholder6.jpg',
+  },
+
+  {
+    img: 'images/front-page/placeholder7.jpg',
+  },
+
+  {
+    img: 'images/front-page/placeholder8.jpg',
+  },
+
+  {
+    img: 'images/front-page/placeholder9.jpg',
+  },
+
+  {
+    img: 'images/front-page/placeholder10.jpg',
+  },
+
+  {
+    img: 'images/front-page/placeholder11.jpg',
+  },
+
+  {
+    img: 'images/front-page/placeholder12.jpg',
+  },
+
+  {
+    img: 'images/front-page/placeholder13.jpg',
+  },
+
+  {
+    img: 'images/front-page/placeholder14.jpg',
+  },
+
+  {
+    img: 'images/front-page/placeholder15.jpg',
+  },
+
+  {
+    img: 'images/front-page/placeholder16.jpg',
+  },
+
+  {
+    img: 'images/front-page/placeholder17.jpg',
+  },
+
+  {
+    img: 'images/front-page/placeholder18.jpg',
+  },
+
+];
+
+'use strict';
+const bgGallery = document.getElementById('background');
+const width = window.innerWidth;
+const height = window.innerHeight;
+
+if (width < 700) {
+  for (let i = 0; i < 4; i++) {
+    bgGallery.innerHTML += `<div class="image-container">
+                            <img src="${imagesArray[i].img}" class="image" alt=" Background Image">
+</div>`;
+  }
+} else {
+
+  for (let i = 0; i < imagesArray.length; i++) {
+    bgGallery.innerHTML += `<div class="image-container">
+                            <img src="${imagesArray[i].img}" class="image" alt=" Background Image">
+</div>`;
+  }
+
+}
+
+
