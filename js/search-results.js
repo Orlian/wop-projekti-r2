cde6f050d1ab60ca5d15d8c48c8f6486207935ff
Joonas Lehtoranta <joonas.lehtoranta@metropolia.fi>
2020-11-24 15:09:02 +0200

    search result hommaa

---
+++ js/search-results.js
@@ -0,0 +1,4 @@
+'use strict';
+
+const searchList = document.querySelector('.search-result-list');
+const searchInput = document.querySelector('.search-bar'); //TODO Varmista search barin nimi kun navbarit valmiit
