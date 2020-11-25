2938175a599b547fbb67c17054f32263d4ca5985
Joonas Lehtoranta <>
2020-11-25 14:30:35 +0200

    Npm ja node asenneltu

--- js/search-results.js
+++ js/search-results.js
@@ -2,3 +2,4 @@
 
 const searchList = document.querySelector('.search-result-list');
 const searchInput = document.querySelector('.search-bar'); //TODO Varmista search barin nimi kun navbarit valmiit
+
