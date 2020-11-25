714885d3ef4c64588826eaf3f24ff3d1649db92b
Joonas Lehtoranta <>
2020-11-25 14:42:12 +0200

    Formatoitu front tavarat public-hakemistoon

--- js/search-results.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-const searchList = document.querySelector('.search-result-list');
-const searchInput = document.querySelector('.search-bar'); //TODO Varmista search barin nimi kun navbarit valmiit
-
---
+++ public/js/search-results.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const searchList = document.querySelector('.search-result-list');
+const searchInput = document.querySelector('.search-bar');
+
+function getSearchResults(){
+  //TODO Hae searchInput.value arvolla modelista kuvat
+}
