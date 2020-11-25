21d8e60bdd62954a30d18584d542051bb716ef6c
Joonas Lehtoranta <>
2020-11-25 19:13:11 +0200

    userModelia ja -controlleria tehty

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,1 +1,34 @@
 'use strict';
+
+const {validationResult} = require('express-validator');
+const userModel = require('/models/userModel');
+//TODO Tänne loput controllerin vaatimat requiret sun muut
+
+const user_list_get = async (req, res) => {
+  const users = await userModel.getAllUsers();
+  res.json(users);
+};
+
+const user_get = async (req, res) => {
+  const email = req.params.email;
+  const user = await userModel.getUser(email);
+  res.json(user);
+};
+
+const user_create = async (req, res) => {
+  console.log('Created user: ', req.body, req.file);
+
+  //TODO Tarkista tietojen järjestys ja tarpeellisuus
+  const {email, password, birthdate, desc, username} = req.body;
+  const params = [email, password, birthdate, desc, username, req.file.filename];
+  await userModel.addUser(params);
+
+  res.json({message: 'upload oukelidoukeli'});
+};
+
+
+module.exports = {
+  user_list_get,
+  user_get,
+  user_create,
+}
--- models/userModel.js
+++ models/userModel.js
@@ -1,1 +1,63 @@
 'use strict';
+
+const pool = require('../database/db');
+const promisePool = pool.promise();
+
+const getAllUsers = async () => {
+  try {
+    const [rows] = await promisePool.query('SELECT email, username FROM user');
+    return rows;
+  } catch (err) {
+    console.log('userModel error', err.message);
+  }
+};
+
+const getUser = async (email) => {
+  try {
+    const [rows] = await promisePool.execute(
+        'SELECT email, username, desc, user_img FROM user WHERE email = ?',
+        [email]);
+    return rows;
+  } catch (err) {
+    console.log('userModel error', err.message);
+  }
+};
+
+const addUser = async (params) => {
+  try {
+    //TODO Varmista sql-lauseen toimivuus valinnaisen datan kanssa
+    const [rows] = await promisePool.execute(
+        'INSERT INTO user (email, password, birthdate, user_img, desc, username) VALUES (?, ?, ?, ?, ?, ?)',
+        params);
+    return rows;
+  } catch (err) {
+    console.log('userModel error', err.message);
+  }
+};
+
+const updateUser = async (params) => {
+  try {
+    const [rows] = promisePool.execute(
+        'UPDATE user SET password = ?, user_img = ?, desc = ?, username = ? WHERE email = ?',
+        params);
+    return rows;
+  } catch (err) {
+    console.log('userModel error', err.message);
+  }
+};
+
+const deleteUser = async (email) => {
+  try {
+    await promisePool.execute('DELETE FROM user WHERE email = ?', [email]);
+  } catch (err) {
+    console.log('userModel error', err.message);
+  }
+};
+
+module.exports = {
+  getAllUsers,
+  getUser,
+  addUser,
+  updateUser,
+  deleteUser,
+}
