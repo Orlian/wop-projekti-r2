ca6a2f90205e2e90ef49fb15e60b98a8c0835b63
Joonas Lehtoranta <>
2020-12-02 12:39:11 +0200

    postModelia korjailtu

--- models/postModel.js
+++ models/postModel.js
@@ -17,7 +17,7 @@ const getAllPosts = async () => {
 
 const getRecentPosts = async () => {
   try {
-    const [rows] = await promisePool.execute('SELECT postid, imgfile, caption, User.username, timestamp FROM Post INNER JOIN User ON Post.email = User.email ORDER BY timestamp LIMIT 50');
+    const [rows] = await promisePool.execute('SELECT postid, imgfile, caption, User.username, timestamp FROM Post INNER JOIN User ON Post.email = User.email ORDER BY timestamp DESC LIMIT 50');
   } catch(err) {
     console.log('postModel error', err.message);
     return {error: 'DB Error'};
@@ -86,4 +86,5 @@ module.exports = {
   addPost,
   updatePost,
   deletePost,
+  getRecentPosts,
 };
