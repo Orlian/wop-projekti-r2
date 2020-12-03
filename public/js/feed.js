7460c9cb4eb4dba652fb109ec7bfce03d8bae997
irina <irina.konovalova455@gmail.com>
2020-12-03 17:10:58 +0200

    filename

--- public/js/feed.js
+++ public/js/feed.js
@@ -15,7 +15,7 @@ const createImageCards = (images) => {
   images.forEach((image) => {
 
     imageFeed.innerHTML += `<div class="card">
-                            <img src="${url + '/uploads/' + image.filename}" class="image" alt="User post">
+                            <img src="${url + '/uploads/' + image.imgfile}" class="image" alt="User post">
                             <div class="aside">
                                 <div class="comments-container">
                                     <div class="img-caption"><p>Caption from db</p></div>
