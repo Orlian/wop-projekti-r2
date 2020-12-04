c3b68dcbcc444fa61cd71f2cbaebfa9cb34d2fa9
35840 <osku1993@luukku.com>
2020-12-04 12:35:55 +0200

    viilausta

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -2,25 +2,26 @@
 const toggle = document.querySelector('.toggle');
 const menu = document.querySelector('.menu');
 const items = document.querySelectorAll('.item');
-const postModal = document.getElementById("post-modal");
+const postModal = document.getElementById('post-modal');
 const editModal = document.getElementById('edit-user-modal');
-const  imageModal = document.querySelector('.image-user-modal');
-const postButtonNav = document.getElementById("add-pic-button");
-const span1 = document.getElementsByClassName("close")[0];
-const span2 = document.getElementsByClassName("close")[1];
-const span3 = document.getElementsByClassName("close")[2];
+const imageModal = document.querySelector('.image-user-modal');
+const postButtonNav = document.getElementById('add-pic-button');
+const postPicture = document.getElementById('art-img');
+const spans = document.querySelectorAll('.close');
+const modals = document.querySelectorAll('.modal');
 const editLink = document.getElementById('edit-link');
 const logoutLink = document.getElementById('logout-link');
-
 
 function toggleMenu() {
   if (menu.classList.contains('active')) {
     menu.classList.remove('active');
-    toggle.querySelector('a').innerHTML = '<ion-icon id="menu-img" name="menu-outline">';
+    toggle.querySelector(
+        'a').innerHTML = '<ion-icon id="menu-img" name="menu-outline">';
   }
   else {
     menu.classList.add('active');
-    toggle.querySelector('a').innerHTML = '<ion-icon name="close-outline"></ion-icon>';
+    toggle.querySelector(
+        'a').innerHTML = '<ion-icon name="close-outline"></ion-icon>';
   }
 
 }
@@ -28,18 +29,20 @@ function toggleMenu() {
 toggle.addEventListener('click', toggleMenu, false);
 
 function toggleItem() {
-  if(this.classList.contains('submenu-active')){
+  if (this.classList.contains('submenu-active')) {
     this.classList.remove('submenu-active');
-  }else if(menu.querySelector('.submenu-active')){
+  }
+  else if (menu.querySelector('.submenu-active')) {
     menu.querySelector('.submenu-active').classList.remove('submenu-active');
     this.classList.add('.submenu-active');
-  }else{
+  }
+  else {
     this.classList.add('submenu-active');
   }
 }
 
 for (let item of items) {
-  if(item.querySelector('.submenu')){
+  if (item.querySelector('.submenu')) {
     item.addEventListener('click', toggleItem, false);
     item.addEventListener('keypress', toggleItem, false);
   }
@@ -48,44 +51,39 @@ for (let item of items) {
 function closeSubmenu(e) {
   let isClickInside = menu.contains(e.target);
 
-  if(!isClickInside && menu.querySelector('.submenu-active')){
+  if (!isClickInside && menu.querySelector('.submenu-active')) {
     menu.querySelector('.submenu-active').classList.remove('submenu-active');
   }
 }
 
 postButtonNav.onclick = function() {
-  postModal.style.display = "flex";
-}
+  postModal.style.display = 'flex';
+};
 
-span1.onclick = function() {
-  postModal.style.display = "none";
-}
+spans.forEach((span) => {
+  span.addEventListener('click', () => {
+    span.parentNode.parentNode.parentNode.parentNode.style.display = 'none';
+  });
+});
 
 editLink.onclick = function() {
-  editModal.style.display = "flex";
-}
-
-span2.onclick = function() {
-  editModal.style.display = "none";
-}
-
-span3.onclick = function() {
-  imageModal.style.display = "none";
-}
-
-
+  editModal.style.display = 'flex';
+};
 
 window.onclick = function(event) {
-  if (event.target === editModal || event.target === postModal) {
-    editModal.style.display = "none";
-    //userPicture.src = ; //TODO hae tietokannasta käyttäjän profiilikuva oletuksena
-    postModal.style.display = "none";
-    postPicture.src = "http://placekitten.com/200/200";
-    imageModal.style.display = "none";
-  }
-}
 
-logoutLink.addEventListener('click',  async (evt) => {
+  modals.forEach((modal)=>{
+    if(event.target === modal){
+      if(modal.id === 'post-modal'){
+        postPicture.src = 'http://placekitten.com/200/200';
+        modal.style.display = 'none';
+      }
+      modal.style.display = 'none';
+    }
+  })
+};
+
+logoutLink.addEventListener('click', async (evt) => {
   evt.preventDefault();
   try {
     const options = {
@@ -98,13 +96,13 @@ logoutLink.addEventListener('click',  async (evt) => {
     console.log('logout json', json);
 
     sessionStorage.removeItem('token');
-    location.href = 'front-page.html'
+    location.href = 'front-page.html';
     alert('You have logged out');
-  } catch(err) {
+  }
+  catch (err) {
     console.log(err.message);
   }
 });
-
 
 document.addEventListener('click', closeSubmenu, false);
 
