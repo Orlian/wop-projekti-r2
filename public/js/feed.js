92e4b246ff246bea3397aadc35b1974248cf84c8
Joonas Lehtoranta <>
2020-12-05 20:18:07 +0200

    fiksaus

--- public/js/feed.js
+++ public/js/feed.js
@@ -169,11 +169,7 @@ const getComments = async (postid) => {
     const response = await fetch(url + '/comment/' + postid, options);
     const comments = await response.json();
     console.log('getComments response', comments);
-    if (comments !== undefined && comments !== '') {
-      return comments;
-    } else {
-      return false;
-    }
+    return comments;
   } catch (e) {
     console.log(e.message);
   }
