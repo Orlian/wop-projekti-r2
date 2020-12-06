7598534b68cf994819ecae86b388c1408abba10e
35840 <osku1993@luukku.com>
2020-12-06 13:30:38 +0200

    viilausta

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -3,6 +3,12 @@ const searchModel = require('../models/searchModel');
 const {validationResult} = require('express-validator');
 
 const search_get_posts = async (req, res) => {
+
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({errors: errors.array()});
+  }
+
   const posts = await searchModel.getSearchResult(req.body.search);
   res.json(posts);
 };
