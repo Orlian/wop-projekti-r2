632384c8cb7a68fc5fa631493c985e121f7bd70f
Joonas Lehtoranta <>
2020-12-07 17:43:39 +0200

    testailua

--- public/js/search-results.js
+++ public/js/search-results.js
@@ -81,16 +81,15 @@ searchForm.addEventListener('submit', async (evt) => {
 
 window.addEventListener('load', async (evt) => {
   evt.preventDefault();
-  const searchParams = {search: params.get('search')};
-  const data = serializeJson(searchParams);
-  console.log('window onload data', data);
+  const searchParams = params.get('search');
+  console.log('window onload data', searchParams);
   const fetchOptions = {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
       'Authorization': 'Bearer ' + sessionStorage.getItem('token'),
     },
-    body: JSON.stringify(data),
+    body: searchParams,
   };
   console.log('window onload body data', fetchOptions.body);
   const response = await fetch(searchUrl + '/search/' + params.get('search'), fetchOptions); //params.search
