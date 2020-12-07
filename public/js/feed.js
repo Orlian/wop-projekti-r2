74d005a0e9b5d6119e1314d8ceec15114ff464ac
Joonas Lehtoranta <>
2020-12-08 01:28:39 +0200

    lazyscroll kokeilu

--- public/js/feed.js
+++ public/js/feed.js
@@ -162,10 +162,11 @@ const getPosts = async () => {
 getPosts();
 
 //Miten ladataan vaan kerran?
-window.addEventListener('scroll', () => {
+window.addEventListener('scroll', async () => {
   let scrollHeight = document.documentElement.scrollHeight;
   if (window.scrollY + window.innerHeight >= scrollHeight - 100) {
-    getPosts();
+    await getPosts();
+
     //scrollHeight - 100  document.documentElement.scrollHeight
   }
 });
