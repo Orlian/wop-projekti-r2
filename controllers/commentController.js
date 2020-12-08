625b8d62cacc446b151075f4d5241a29ff0f9a07
35840 <osku1993@luukku.com>
2020-12-08 18:35:20 +0200

    viilausta

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -22,7 +22,7 @@ const post_comment_delete = async (req, res) => {
 
 const get_commenter = async (req,res) => {
   const params = [req.params.postid, req.user.userid];
-  await commentModel.getCommenter(params);
+  const commenter = await commentModel.getCommenter(params);
   res.json(commenter);
 }
 
