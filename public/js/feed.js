e734ca646c759cabd594e92698b2409a19ae20e7
Joonas Lehtoranta <>
2020-12-08 17:18:11 +0200

    comment add

--- public/js/feed.js
+++ public/js/feed.js
@@ -104,7 +104,7 @@ const createImageCards = (images) => {
       try {
         const response = await fetch(url + '/comment/'+ image.postid, fetchOptions);
         const comment = await response.json();
-        window.reload(true);
+        location.reload();
       } catch (err) {
         console.log(err.message);
       }
