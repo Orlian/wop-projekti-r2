2f1ed3cd8e128a8d3239acf9d31f402ac5fa17ef
Joonas Lehtoranta <>
2020-12-09 10:10:50 +0200

    fiksailua

--- models/userModel.js
+++ models/userModel.js
@@ -59,18 +59,22 @@ const getUserLogin = async (params) => {
 };
 
 const updateUser = async (params) => {
+  const newParams = [];
   try {
     let lause = 'UPDATE User SET ';
     if(params[0] !== undefined){
       lause += 'password=?, ';
+      newParams.push(params[0]);
     }
     if(params[1] !== ''){
       lause += 'userimg=?, ';
+      newParams.push(params[1]);
     }
+    newParams.push(params[2]);
+    newParams.push(params[3]);
     console.log('updateUser lause', lause, '\nupdateUser params', params);
     const [rows] = promisePool.execute(
-        lause +'description=? WHERE userid = ?',
-        params);
+        lause +'description=? WHERE userid = ?', newParams);
     return rows;
   } catch (err) {
     console.log('userModel error', err.message);
