5b8b0441dfd5414d1035c49e1db059a0c6de08ae
35840 <osku1993@luukku.com>
2020-12-09 17:38:28 +0200

    bugin korjausta

--- public/js/profile.js
+++ public/js/profile.js
@@ -96,6 +96,7 @@ const getUserProfile = async () => {
     const response = await fetch(url + '/user/' + user.email,
         fetchOptions);
     const userData = await response.json();
+    console.log(userData, 'jotain');
     profileImg.src = url + '/thumbnails/' + userData.userimg;
     profileName.innerHTML = userData.username;
     profileDesc.innerHTML = userData.description;
