c02f61b4dcb5fbdd6351c87dfb7fb392e6cb53ae
irina <irina.konovalova455@gmail.com>
2020-12-10 19:14:13 +0200

    css viimeistelyä

--- public/js/search-results.js
+++ public/js/search-results.js
@@ -78,6 +78,10 @@ const fillSearchList = (hits) => {
         commenters.forEach((commenter) => {
               if (commenter.commentid === comment.commentid) {
                 const deleteCommentButton = document.createElement('button');
+                const deleteCommentIcon = document.createElement('ion-icon');
+                deleteCommentIcon.classList.add('delete-comment-icon');
+                deleteCommentIcon.name = 'close-outline';
+                deleteCommentButton.appendChild(deleteCommentIcon);
                 commentLi.appendChild(deleteCommentButton);
                 deleteCommentButton.id = 'delete-comment-button';
                 deleteCommentButton.addEventListener('click', async (evt) => {
